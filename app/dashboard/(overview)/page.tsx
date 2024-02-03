81b1a47872d23a516ffc6e3edfddbc974c014267
Érico Bis <ericobis.santos@gmail.com>
2024-02-02 21:32:39 -0300

    add | metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,6 +1,11 @@
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
